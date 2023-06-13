af24da12b02c68ac3d6f8bad20299d5ecf5fcfcd
Mikhail Chekushkin <rbmihey@mail.ru>
2023-06-14 00:43:16 +0500

    Add task2

---
+++ task2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+  int num = Random().Next(10,100);  
+
+Console.Writeline("Ваше число: ");
+
+int Max (int number)
+
+{    
+    if (number % 10 > number / 10)
+            return true;
+    
+    System.Console.WriteLine("Число не трехзначное");
+    return false;
+}
